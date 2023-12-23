afcee3c7365500a1394ac84aaf88810a38749185
Agnel Francis Olakkengil <agnelfrancis2007@hotmail.com>
2023-12-23 11:45:07 +0530

    Changed Icons In Others And Linked Webs

--- src/Others.js
+++ src/Others.js
@@ -1,40 +1,35 @@
 // HomeServ.js
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Linking } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons'; // Import the icon library
 
 const HomeServ = () => {
   const serviceButtons = [
-    { label: 'KSEB', value: 'kseb', link: 'https://wss.kseb.in/selfservices/quickpay' },
-    { label: 'KSFE', value: 'ksfe', link: 'https://ksfeonline.com/payment'},
-    { label: 'Akshaya Govt.', value: 'akshayaGovt', link: 'http://www.akshaya.kerala.gov.in/' },
-    { label: 'Post Office', value: 'postOffice', link:'https://www.indiapost.gov.in/'},
-    { label: 'Courier', value: 'courier', link: 'https://www.dhl.com/' },
-    { label: 'Bank', value: 'bank', link: 'https://www.rbi.org.in/' },
-    { label: 'Hospital', value: 'hospital', link: 'https://dhs.kerala.gov.in/' },
-    { label: 'Edu. Instit.', value: 'eduInstitute', link: 'https://education.kerala.gov.in/' },
-    { label: 'Online Shops', value: 'shops', link: 'https://www.amazon.in/' },
-    { label: 'Medical Store', value: 'medicalStore', link: 'https://www.quickerala.com/thrissur/healthcare/medical-shops/sbct-5801-dt-13' },
-    { label: 'Religious Places', value: 'religiousPlaces', link: 'http://tinyurl.com/religiousplaces' },
-    { label: 'Hotels', value: 'hotels', link: 'http://tinyurl.com/363e2vau' },
-    { label: 'Job Vacancies', value: 'jobVacancies', link: 'https://www.google.com/about/careers/applications/jobs/results/?location=India' },
-    { label: 'Media', value: 'media', link: 'https://www.manoramaonline.com/' },
-    { label: 'Agri/Vet Offices', value: 'agriVetOffices', link: 'http://tinyurl.com/agrivetoffices' },
-    { label: 'Restaurants', value: 'restaurants', link: 'http://tinyurl.com/bdvuw2hj' },
+    { label: 'KSEB', value: 'kseb', link: 'https://wss.kseb.in/selfservices/quickpay', icon: 'ios-flash-outline' },
+    { label: 'KSFE', value: 'ksfe', link: 'https://ksfeonline.com/payment', icon: 'ios-cash-outline' },
+    { label: 'Akshaya Govt.', value: 'akshayaGovt', link: 'http://www.akshaya.kerala.gov.in/', icon: 'ios-globe-outline' },
+    { label: 'Post Office', value: 'postOffice', link: 'https://www.indiapost.gov.in/', icon: 'ios-mail-outline' },
+    { label: 'Courier', value: 'courier', link: 'https://www.dhl.com/', icon: 'ios-archive-outline' },
+    { label: 'Bank', value: 'bank', link: 'https://www.rbi.org.in/', icon: 'ios-briefcase-outline' },
+    { label: 'Hospital', value: 'hospital', link: 'https://dhs.kerala.gov.in/', icon: 'ios-medkit-outline' },
+    { label: 'Edu. Instit.', value: 'eduInstitute', link: 'https://education.kerala.gov.in/', icon: 'ios-book-outline' },
+    { label: 'Online Shops', value: 'shops', link: 'https://www.amazon.in/', icon: 'ios-cart-outline' },
+    { label: 'Medical Store', value: 'medicalStore', link: 'https://www.quickerala.com/thrissur/healthcare/medical-shops/sbct-5801-dt-13', icon: 'ios-medical-outline' },
+    { label: 'Religious Places', value: 'religiousPlaces', link: 'http://tinyurl.com/religiousplaces', icon: 'ios-star-outline' },
+    { label: 'Hotels', value: 'hotels', link: 'http://tinyurl.com/363e2vau', icon: 'ios-bed-outline' },
+    { label: 'Jobs', value: 'jobVacancies', link: 'https://www.google.com/about/careers/applications/jobs/results/?location=India', icon: 'ios-briefcase-outline' },
+    { label: 'Media', value: 'media', link: 'https://www.manoramaonline.com/', icon: 'ios-newspaper-outline' },
+    { label: 'Agri/Vet Offices', value: 'agriVetOffices', link: 'http://tinyurl.com/agrivetoffices', icon: 'ios-leaf-outline' },
+    { label: 'Food', value: 'restaurants', link: 'http://tinyurl.com/bdvuw2hj', icon: 'ios-restaurant-outline' },
   ];
 
   const handleButtonPress = (item) => {
-    // Handle button press based on the item value
     console.log(`Button pressed: ${item.label}`);
 
-    // Check if a link is provided for the button
     if (item.link) {
-      // Show a redirect message (you can customize this message)
       console.log(`Redirecting to: ${item.link}`);
-      
-      // Open the web link
       Linking.openURL(item.link).catch((err) => console.error('Error opening link:', err));
     }
-    // You can add additional logic for other buttons here
   };
 
   return (
@@ -46,10 +41,13 @@ const HomeServ = () => {
         {serviceButtons.map((item) => (
           <TouchableOpacity
             key={item.value}
-            style={[styles.button, { backgroundColor: '#001F3F' }]}
+            style={[styles.button, { backgroundColor: 'transparent', borderColor: '#001F3F', borderWidth: 1 }]}
             onPress={() => handleButtonPress(item)}
           >
-            <Text style={[styles.buttonText, { fontFamily: 'Poppins-Medium' }]}>{item.label}</Text>
+            {/* Use the specified icons for each button */}
+            {item.icon && <Icon name={item.icon} size={30} color="#001F3F" />}
+
+            <Text style={[styles.buttonText, { fontFamily: 'Poppins-Medium', marginTop: 5, color: '#001F3F' }]}>{item.label}</Text>
           </TouchableOpacity>
         ))}
       </View>
@@ -70,20 +68,21 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     width: '80%',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
   button: {
-    backgroundColor: '#001F3F', // Dark blue color
     padding: 15,
     marginVertical: 10,
-    width: '100%',
+    width: '30%',
+    aspectRatio: 1,
     borderRadius: 25,
     justifyContent: 'center',
     alignItems: 'center',
   },
   buttonText: {
-    color: 'white',
     fontSize: 16,
     fontWeight: 'bold',
   },
